5ef110ac9fe75a70ebd596c5f605e1795e2972cd
Shreyas Ramkumar <101288239+ShreyasRamkumar@users.noreply.github.com>
2024-03-13 14:04:30 -0400

    Update augmentation.py

--- code/augmentation.py
+++ code/augmentation.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 class Augmentation():
     def __init__(self, input_folder, output_folder):
-        self.input_folder = f"{input_folder}/anat/"
+        self.input_folder = f"{input_folder}"
         self.output_folder = output_folder
         self.augmentations = [RandomMotion(degrees=randint(1, 6), translation=randint(1, 6), num_transforms=randint(2, 4)), 
                               RandomGhosting(num_ghosts=randint(4, 10), intensity=randint(0, 3)), 
@@ -20,10 +20,10 @@ class Augmentation():
             index = randint(0, len(self.augmentations) - 1) 
             selected_augmentations.append(self.augmentations[index]) 
         transforms = Compose(selected_augmentations)
-        for i in tqdm(listdir(self.input_folder)):
-            image = ScalarImage(f"{self.input_folder}/{i}/anat/{i}_T1w.nii")
+        for j in tqdm(listdir(self.input_folder)):
+            image = ScalarImage(f"{self.input_folder}/{j}/anat/{j}_T1w.nii")
             augmented_image = transforms(image)
-            augmented_image.save(f"{self.output_folder}/{i}")
+            augmented_image.save(f"{self.output_folder}/{j}/anat/{j}_T1w.nii")
 
 if __name__ == "__main__":
     for i in listdir("./data"):
