31478cd000b74d351ccad5ae936864c9a57a0163
Shreyas Ramkumar <101288239+ShreyasRamkumar@users.noreply.github.com>
2024-03-15 01:42:25 -0400

    fixed data loading for network

--- code/3d_network.py
+++ code/3d_network.py
@@ -6,11 +6,55 @@ from torch.utils.data import DataLoader, Dataset
 import nibabel as nib
 import numpy as np
 from lightning.pytorch.callbacks import Callback
-import network_utility as Network_Utility
+from torch import optim, nn, ones, sqrt, mean, std, abs
+import torch.nn.functional as F
+from tqdm import tqdm
 
-scanner_1_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/scanner_1/" # CHANGE FOR WHEN USING JENKINS
-scanner_2_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/scanner_2/" # CHANGE FOR WHEN USING JENKINS
-harmonized_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/harmonized/" # CHANGE FOR WHEN USING JENKINS
+scanner_1_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/scanner_1_processed" # CHANGE FOR WHEN USING JENKINS
+scanner_2_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/scanner_2_processed" # CHANGE FOR WHEN USING JENKINS
+harmonized_directory = "/home/ramkumars@acct.upmchs.net/Projects/3D-Harmonization/data/harmonized" # CHANGE FOR WHEN USING JENKINS
+
+class Network_Utility():
+    @staticmethod
+    def conv_3d(in_c, out_c):
+        run = nn.Sequential(
+            nn.Conv3d(in_channels=in_c, out_channels=out_c, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm3d(num_features=out_c)
+        )
+        return run
+
+    @staticmethod
+    def down_conv_3d(in_c, out_c):
+        run = nn.Sequential(
+            nn.Conv3d(in_channels=in_c, out_channels=out_c, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm3d(num_features=out_c)
+        )
+        return run
+        
+    @staticmethod
+    def up_conv_3d(in_c, out_c):
+        run = nn.Sequential(
+            nn.ConvTranspose3d(in_channels=in_c, out_channels=out_c, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm3d(num_features=out_c),
+        )
+        return run
+
+    @staticmethod
+    def final_conv_3d(in_c, out_c):
+        run = nn.Sequential(
+            nn.Conv3d(in_channels=in_c, out_channels=out_c, kernel_size=1),
+            nn.ReLU()
+        )
+        return run
+    
+    @staticmethod
+    def create_data_splits(dataset_len):
+        training_len = int(dataset_len * 0.8)
+        validation_len = int((dataset_len - training_len) / 2)
+        return [training_len, validation_len, validation_len]
 
 # Model Class
 class Unet(pl.LightningModule):
@@ -163,14 +207,12 @@ class MRIDataset(Dataset):
 
     def __getitem__(self, index):
         scan_path = self.model_input[index]
-        intermediate_scan_list = os.listdir(f"{scanner_1_directory}/{scan_path}/anat/")
-        scan = nib.load(f"{scanner_1_directory}/{scan_path}/anat/{intermediate_scan_list[0]}")
+        scan = nib.load(f"{scanner_1_directory}/{scan_path}")
         scan_array = scan.get_fdata()
         scan_tensor = torch.tensor(scan_array, dtype=torch.float32)
 
         ground_truth_scan_path = self.ground_truth[index]
-        intermediate_scan_list = os.listdir(f"{scanner_1_directory}/{ground_truth_scan_path}/anat/")
-        ground_truth_scan = nib.load(f"{scanner_1_directory}/{ground_truth_scan_path}/anat/{intermediate_scan_list[0]}")
+        ground_truth_scan = nib.load(f"{scanner_2_directory}/{ground_truth_scan_path}")
         ground_truth_scan_array = ground_truth_scan.get_fdata()
         ground_truth_scan_tensor = torch.tensor(ground_truth_scan_array, dtype=torch.float32)
 
